fd2b9b3128fd350cfa7284c81646a5f7eeb15ba8
Samuel Martinez <smartinez9535@gmail.com>
2021-08-03 10:55:48 -0600

    W1D2 Algos

--- algos/algo.js
+++ algos/algo.js
@@ -9,3 +9,64 @@ for (let i of [7,3,5,2]) {
 console.log(head.val); //1
 console.log(head.next.val); //7
 console.log(head.next.next.next.val); // 5
+
+/*------------------------------------------------------------------------ */
+
+//  TUESDAY -------------------------------------------------------------------//
+    /**
+     * Prints all the nodes in the list to the console.
+     * of this list.
+     * - Time: (?).
+     * - Space: (?).
+     * @returns {SinglyLinkedList} This list.
+     */
+    printValues() {
+        if(this.head == null){
+            console.log("LIST EMPTY")
+            return
+        }
+        // IF THE LIST IS NOT EMPTY
+        var runner = this.head
+        while(runner != null){
+            console.log(runner.value)
+            runner = runner.next
+        }
+    }
+    /**
+     * Creates a new node with the given data and inserts it at the back of
+     * this list.
+     * - Time: (?).
+     * - Space: (?).
+     * @param {any} data The data to be added to the new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+    insertAtBack(data){
+        if(this.head == null){
+            this.head = new SLNode(data);
+        }
+
+        else{
+            let runner = this.head
+            while(runner != null){
+                runner = runner.next
+            }
+            runner.next = newSLNode(data);
+        }
+        return this.head
+        // 1. SLL IS EMPTY
+        // 2. SLL IS NOT EMPTY
+    }
+    
+    /**
+     * Adds each item of the given array to the back of this list.
+     * - Time: (?).
+     * - Space: (?).
+     * @param {Array<any>} vals The data for each new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+    seedFromArr(vals) {
+        for(val of vals){
+            insertAtBack(val);
+        }
+        return this.head;
+    }
